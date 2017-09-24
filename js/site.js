a9e3c14a1cda1a77f30db78ac9997fa4f5ee35f6
Sanju Ashok <srinivas.ashok@live.com>
2017-09-24 12:58:45 -0700

    adding slides and scroll reveal to site

--- js/site.js
+++ js/site.js
@@ -1,10 +1,15 @@
 console.log("loading site");
 $( document ).ready(function() {
     $("#links a").hover(function (){
-            console.log("loading function");
             $(this).css("text-decoration", "underline");
         },function(){
             $(this).css("text-decoration", "none");
         }
     );
+
+    window.sr = ScrollReveal({ reset: true });
+    sr.reveal('#about', { duration: 400 });
+    sr.reveal('#sounds', { duration: 400 });
+    sr.reveal('#cooking', { duration: 400 });
+    sr.reveal('#fitness', { duration: 400 });
 });
